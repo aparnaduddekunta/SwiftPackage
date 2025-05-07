595d789104bb5105e75a16f4fce0b2b1163569e6
Aparna <aparna.duddekunta@ibm.com>
2025-05-07 19:58:01 +0530

    Initial commit

---
+++ Sources/SwiftPackage/RoyalButton.swift
@@ -0,0 +1,28 @@
+//
+//  RoyalButton.swift
+//  SwiftPackage
+//
+//  Created by Aparna Duddekunta on 07/05/25.
+//
+import SwiftUI
+
+
+public struct RoyalButton: View {
+    public init(){
+        
+    }
+    public var body: some View {
+        VStack(alignment: .leading) {
+            Button("Show details") {
+                //showDetails.toggle()
+            }
+
+
+        }
+    }
+}
+
+#Preview {
+    RoyalButton()
+}
+
